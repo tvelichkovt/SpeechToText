199c22c0616d03669db9687883829fac8f74ba40
Todor Velichkov <tvelichkovt@gmail.com>
2020-03-10 09:22:02 +0200

    Create SpeechToText.py

---
+++ SpeechToText.py
@@ -0,0 +1,17 @@
+#pip install SpeechRecognition
+
+import speech_recognition as sr
+r = sr.Recognizer()
+
+soundfile=sr.AudioFile('test_short.wav')
+with soundfile as source:
+    audio = r.record(source)
+try:
+    soundfile_text = r.recognize_google(audio)
+    print("Text: "+soundfile_text)
+except Exception as e:
+    print("Exception: "+str(e))
+    
+with open('soundfile_text.txt', 'w') as f: #, encoding="utf-8") for Bulgarian
+    print(soundfile_text, file=f)
+
